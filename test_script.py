91202478e101d69e89df0388e1aabf7f9a2fc0d4
scbx <32655360+scbx@users.noreply.github.com>
2019-06-12 15:55:44 +1000

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,10 @@
+from FortiAPI import *
+
+return_lists = []
+with open('testfile.py', 'r') as f:
+    return_lists = f.read().splitlines()
+
+x = Fortigate(ip='192.168.1.99', vdom='root', user='admin', passwd='')
+for i in return_lists:
+    i = i.split(',')
+    print(x.AddFwAddressIdempotent(name=+i[0]+,subnet=i[1]))
