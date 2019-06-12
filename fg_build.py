5a2eff938276f599cb32d0b65b31b342e4e1718f
scbx <32655360+scbx@users.noreply.github.com>
2019-06-12 15:45:34 +1000

    Create fg_build.py

---
+++ fg_build.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+fg_default = {
+    'device_type':'fortinet_ssh',
+    'ip':'192.168.1.99',
+    'username':'admin',
+    'password':''
+}
+
+miko_devices = [fg_default]
+
+with open('netmiko_preconfig.txt') as f:
+    lines = f.read().splitlines()
+
+for device in miko_devices:
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_config_set(lines)
+    print(output)
